f58132d2e5394485ba0735e1852b7baf46a30a20
mariafergp <mariainesfgp@gmail.com>
2017-11-24 18:16:07 -0300

    update gem file and migrations

--- db/migrate/20171119203430_rename_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameTable < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :authorization_tables, :authorization
-  end
-end
--- db/migrate/20171119203807_rename_table_cost_zone.rb
+++
@@ -1,5 +0,0 @@
-class RenameTableCostZone < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :cost_zones, :costzones
-  end
-end
