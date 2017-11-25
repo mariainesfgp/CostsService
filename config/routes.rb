b64a197cfc67797f34b5a4e9fc3784ebbf6e6461
mariafergp <mariainesfgp@gmail.com>
2017-11-25 10:53:16 -0300

    add initializers and schedule.yml for sidekiq configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+    require 'sidekiq/web'
+    require 'sidekiq/cron/web'
+    mount Sidekiq::Web => '/sidekiq'
+
     get 'costs/prueba', to: 'costs#prueba'
     get 'costs/calculate_cost', to:'costs#calculate_cost'
     get 'costs/is_updated', to:'costs#is_updated'
