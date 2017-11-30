8227085fc1c8fc3c074e73fe10a00b5dc56260df
Unknown <mg27ago@gmail.com>
2017-11-30 19:48:41 -0300

    Configuracion
    Configuracion

--- config/initializers/attributes_env.rb
+++ config/initializers/attributes_env.rb
@@ -1,10 +1,7 @@
-if Rails.env.development?
-  ENV['user']='hola'
-  ENV['password'] = 'hola'
-  ENV['URICost'] = 'https://delivery-rates.mybluemix.net/cost'
-  ENV['URIAreas']='https://delivery-rates.mybluemix.net/areas'
-  ENV['AuthDeliveryRateAccount'] = '146507'
-  ENV['AuthDeliveryRatePassword'] = 'oCwSHoEeVlZS'
-  ENV['URIDeliveryRates'] = 'https://delivery-rates.mybluemix.net/'
-
-end
+ENV['user']='hola'
+ENV['password'] = 'hola'
+ENV['URICost'] = 'https://delivery-rates.mybluemix.net/cost'
+ENV['URIAreas']='https://delivery-rates.mybluemix.net/areas'
+ENV['AuthDeliveryRateAccount'] = '146507'
+ENV['AuthDeliveryRatePassword'] = 'oCwSHoEeVlZS'
+ENV['URIDeliveryRates'] = 'https://delivery-rates.mybluemix.net/'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-
     require 'sidekiq/web'
     require 'sidekiq/cron/web'
     mount Sidekiq::Web => '/sidekiq'
-
     get 'application/healthCheck', to: 'application#health_check'
-    get 'costs/calculate_cost', to:'costs#calculate_cost'
-    get 'costs/is_updated', to:'costs#is_updated'
+    get 'costs/calculate_cost', to: 'costs#calculate_cost'
+    get 'costs/is_updated', to: 'costs#is_updated'
 end
